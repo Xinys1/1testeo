9d3a8c54d8fad06177a90f6add05411fbc095412
Xinys1 <joseasensiobarrios@gmail.com>
2025-03-18 13:08:26 +0100

    Ajusté el modal de Contacto para que sea centrado, responsivo, con esquinas redondeadas y estilo oscuro

--- Contacto/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.getElementById('feedback-form').addEventListener('submit', function (e) {
-    e.preventDefault();  // Evitar el envío real del formulario
-
-    // Aquí se podrían realizar validaciones adicionales si se desea.
-
-    alert("¡Gracias por tu feedback! Hemos recibido tu respuesta.");
-});
